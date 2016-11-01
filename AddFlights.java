c6b8c0cfef4dd27b64c524445522cd5c2fa916c0
danadai <1265375678@qq.com>
2016-10-31 22:03:51 -0400

    Update AddFlights.java

--- AddFlights.java
+++ AddFlights.java
@@ -1,10 +1,20 @@
+/**CS603 - Assignment 4
+ * This	class has a	single main method that provides the following functionalities with Object Flight
+ * @author Danna Dai
+ *
+ */
 import java.util.Scanner;
 
 public class AddFlights {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		Flight[] flightArray = new Flight[2];
-
+		Flight[] flightArray = new Flight[2];// define an array of type Flight
+												// of size 2
+		/**
+		 * Prompt the user for the information required to create a Flight
+		 * object in the following order: flight code, departure city, arrival
+		 * city, and the length of the flight.
+		 */
 		for (int i = 0; i < flightArray.length; i++) {
 			String flightCode;
 			String from;
@@ -14,7 +24,8 @@ public class AddFlights {
 
 			System.out.print(
 					"Please enter a flight code that begins with two letters followed by a space and then 2 to 4 digits: ");
-			if (i!=0) input.next();
+			if (i != 0)
+				input.next();
 			flightCode = input.nextLine();
 
 			System.out.print("Enter the departure city: ");
@@ -25,20 +36,29 @@ public class AddFlights {
 
 			System.out.print("Enter the flight duration in minutes: ");
 			length = input.nextInt();
-			
+			/**
+			 * Create the Flight object with the values input by the user and
+			 * print its description
+			 */
 			Flight flight = new Flight(flightCode, from, to, length);
-			
+
 			System.out.println();
 			System.out.println(flight);
 			System.out.println();
-			
-			
-			while (Flight.validateCode(flight.getFlightCode()).equals("Unassigned")) {			
+			/**
+			 * Get the flight code of the newly created Flight object and use
+			 * the static validateCode method to check if it is “unassigned.” If
+			 * it is, continue to prompt until a valid code has been entered,
+			 * and then set the flight code for the flight to this new value.
+			 * Then print the description of the flight again.
+			 * 
+			 */
+			while (Flight.validateCode(flight.getFlightCode()).equals("Unassigned")) {
 				System.out.print("The flight code for the newly created flight is invalid. Please re-enter: ");
 				input.next();
 				flightCode = input.nextLine();
+				flight.setFlightCode(flightCode);
 				changed = true;
-				flight = new Flight(flightCode, from, to, length);
 			}
 			if (changed) {
 				System.out.println();
@@ -46,8 +66,15 @@ public class AddFlights {
 				System.out.println();
 
 			}
+
 			flightArray[i] = flight;
+			// store the two Flight objects to
+			// an array of type Flight
 		}
+		/**
+		 * Print if the first Flight object connects to the second Flight
+		 * object, making use of the connectsTo() method.
+		 */
 		if (flightArray[1].connectsTo(flightArray[0]))
 			System.out.println("The two flights are connecting.");
 		else
